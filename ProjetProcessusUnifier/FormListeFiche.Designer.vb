33e337eb19baa38315f00969edeafd0479be35ce
AbdouRahimeDaouda <abdourahime336@gmail.com>
2019-05-09 15:06:32 +0100

    Ajout label test

--- ProjetProcessusUnifier/FormListeFiche.Designer.vb
+++ ProjetProcessusUnifier/FormListeFiche.Designer.vb
@@ -35,6 +35,7 @@ Partial Class FormListeFiche
         Me.REPONSE1 = New System.Windows.Forms.DataGridViewTextBoxColumn()
         Me.VILLE = New System.Windows.Forms.DataGridViewTextBoxColumn()
         Me.REPONSE4 = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.Label2 = New System.Windows.Forms.Label()
         CType(Me.pbxAtt, System.ComponentModel.ISupportInitialize).BeginInit()
         CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
@@ -45,9 +46,10 @@ Partial Class FormListeFiche
         Me.btnVoirFiche.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
         Me.btnVoirFiche.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.btnVoirFiche.ForeColor = System.Drawing.Color.White
-        Me.btnVoirFiche.Location = New System.Drawing.Point(625, 352)
+        Me.btnVoirFiche.Location = New System.Drawing.Point(833, 433)
+        Me.btnVoirFiche.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
         Me.btnVoirFiche.Name = "btnVoirFiche"
-        Me.btnVoirFiche.Size = New System.Drawing.Size(108, 31)
+        Me.btnVoirFiche.Size = New System.Drawing.Size(144, 38)
         Me.btnVoirFiche.TabIndex = 106
         Me.btnVoirFiche.Text = "Voir fiche"
         Me.btnVoirFiche.UseVisualStyleBackColor = True
@@ -56,9 +58,10 @@ Partial Class FormListeFiche
         '
         Me.pbxAtt.BackgroundImage = Global.ProjetProcessusUnifier.My.Resources.Resources.avertissement_signe_exclamation_pression_mark_jaune_icone_feux_erreur_point_lattention_vector_triangle_symbole_isole_gwgdkp
         Me.pbxAtt.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
-        Me.pbxAtt.Location = New System.Drawing.Point(6, 345)
+        Me.pbxAtt.Location = New System.Drawing.Point(8, 425)
+        Me.pbxAtt.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
         Me.pbxAtt.Name = "pbxAtt"
-        Me.pbxAtt.Size = New System.Drawing.Size(16, 38)
+        Me.pbxAtt.Size = New System.Drawing.Size(21, 47)
         Me.pbxAtt.TabIndex = 104
         Me.pbxAtt.TabStop = False
         '
@@ -67,9 +70,10 @@ Partial Class FormListeFiche
         Me.lblAtt.BackColor = System.Drawing.Color.Transparent
         Me.lblAtt.Font = New System.Drawing.Font("Times New Roman", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.lblAtt.ForeColor = System.Drawing.Color.FromArgb(CType(CType(192, Byte), Integer), CType(CType(192, Byte), Integer), CType(CType(0, Byte), Integer))
-        Me.lblAtt.Location = New System.Drawing.Point(19, 342)
+        Me.lblAtt.Location = New System.Drawing.Point(25, 421)
+        Me.lblAtt.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
         Me.lblAtt.Name = "lblAtt"
-        Me.lblAtt.Size = New System.Drawing.Size(210, 45)
+        Me.lblAtt.Size = New System.Drawing.Size(280, 55)
         Me.lblAtt.TabIndex = 103
         Me.lblAtt.Text = "Selectionnr une fiche avant de cliquer sur un bouton" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10)
         Me.lblAtt.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
@@ -79,9 +83,10 @@ Partial Class FormListeFiche
         Me.Label1.BackColor = System.Drawing.Color.Transparent
         Me.Label1.Font = New System.Drawing.Font("Modern No. 20", 20.25!, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.Label1.ForeColor = System.Drawing.Color.White
-        Me.Label1.Location = New System.Drawing.Point(1, 20)
+        Me.Label1.Location = New System.Drawing.Point(1, 25)
+        Me.Label1.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
         Me.Label1.Name = "Label1"
-        Me.Label1.Size = New System.Drawing.Size(279, 29)
+        Me.Label1.Size = New System.Drawing.Size(372, 36)
         Me.Label1.TabIndex = 102
         Me.Label1.Text = "Liste des fiches patients "
         '
@@ -93,14 +98,15 @@ Partial Class FormListeFiche
         Me.DataGridView1.BackgroundColor = System.Drawing.SystemColors.ActiveCaption
         Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
         Me.DataGridView1.Columns.AddRange(New System.Windows.Forms.DataGridViewColumn() {Me.ID, Me.Column1, Me.REPONSE2, Me.NOM, Me.REPONSE1, Me.VILLE, Me.REPONSE4})
-        Me.DataGridView1.Location = New System.Drawing.Point(6, 51)
+        Me.DataGridView1.Location = New System.Drawing.Point(8, 63)
+        Me.DataGridView1.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
         Me.DataGridView1.MultiSelect = False
         Me.DataGridView1.Name = "DataGridView1"
         Me.DataGridView1.ReadOnly = True
         DataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.[True]
         Me.DataGridView1.RowsDefaultCellStyle = DataGridViewCellStyle1
         Me.DataGridView1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect
-        Me.DataGridView1.Size = New System.Drawing.Size(727, 291)
+        Me.DataGridView1.Size = New System.Drawing.Size(969, 358)
         Me.DataGridView1.TabIndex = 101
         '
         'ID
@@ -149,23 +155,35 @@ Partial Class FormListeFiche
         Me.REPONSE4.Name = "REPONSE4"
         Me.REPONSE4.ReadOnly = True
         '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(512, 211)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(49, 17)
+        Me.Label2.TabIndex = 107
+        Me.Label2.Text = "Abdou"
+        '
         'FormListeFiche
         '
-        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.BackgroundImage = Global.ProjetProcessusUnifier.My.Resources.Resources.degrade_gris_bleu_1095_593
         Me.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
-        Me.ClientSize = New System.Drawing.Size(738, 405)
+        Me.ClientSize = New System.Drawing.Size(984, 498)
+        Me.Controls.Add(Me.Label2)
         Me.Controls.Add(Me.btnVoirFiche)
         Me.Controls.Add(Me.pbxAtt)
         Me.Controls.Add(Me.lblAtt)
         Me.Controls.Add(Me.Label1)
         Me.Controls.Add(Me.DataGridView1)
+        Me.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
         Me.Name = "FormListeFiche"
         Me.Text = "FormListeFiche"
         CType(Me.pbxAtt, System.ComponentModel.ISupportInitialize).EndInit()
         CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
+        Me.PerformLayout()
 
     End Sub
 
@@ -181,4 +199,5 @@ Partial Class FormListeFiche
     Friend WithEvents REPONSE1 As DataGridViewTextBoxColumn
     Friend WithEvents VILLE As DataGridViewTextBoxColumn
     Friend WithEvents REPONSE4 As DataGridViewTextBoxColumn
+    Friend WithEvents Label2 As Label
 End Class
