73bc635e13cfca9dd72d5829729f404f902f5ce2
AbdouRahimeDaouda <abdourahime336@gmail.com>
2019-05-09 15:38:46 +0100

    Ajout lab 1

--- ProjetProcessusUnifier/FormListeFiche.Designer.vb
+++ ProjetProcessusUnifier/FormListeFiche.Designer.vb
@@ -22,7 +22,7 @@ Partial Class FormListeFiche
     'Ne la modifiez pas à l'aide de l'éditeur de code.
     <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
-        Dim DataGridViewCellStyle3 As System.Windows.Forms.DataGridViewCellStyle = New System.Windows.Forms.DataGridViewCellStyle()
+        Dim DataGridViewCellStyle1 As System.Windows.Forms.DataGridViewCellStyle = New System.Windows.Forms.DataGridViewCellStyle()
         Me.btnVoirFiche = New System.Windows.Forms.Button()
         Me.pbxAtt = New System.Windows.Forms.PictureBox()
         Me.lblAtt = New System.Windows.Forms.Label()
@@ -36,6 +36,7 @@ Partial Class FormListeFiche
         Me.VILLE = New System.Windows.Forms.DataGridViewTextBoxColumn()
         Me.REPONSE4 = New System.Windows.Forms.DataGridViewTextBoxColumn()
         Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
         CType(Me.pbxAtt, System.ComponentModel.ISupportInitialize).BeginInit()
         CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
@@ -103,8 +104,8 @@ Partial Class FormListeFiche
         Me.DataGridView1.MultiSelect = False
         Me.DataGridView1.Name = "DataGridView1"
         Me.DataGridView1.ReadOnly = True
-        DataGridViewCellStyle3.WrapMode = System.Windows.Forms.DataGridViewTriState.[True]
-        Me.DataGridView1.RowsDefaultCellStyle = DataGridViewCellStyle3
+        DataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.[True]
+        Me.DataGridView1.RowsDefaultCellStyle = DataGridViewCellStyle1
         Me.DataGridView1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect
         Me.DataGridView1.Size = New System.Drawing.Size(969, 358)
         Me.DataGridView1.TabIndex = 101
@@ -164,6 +165,15 @@ Partial Class FormListeFiche
         Me.Label2.TabIndex = 107
         Me.Label2.Text = "Label2"
         '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(346, 239)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(51, 17)
+        Me.Label3.TabIndex = 108
+        Me.Label3.Text = "Label3"
+        '
         'FormListeFiche
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
@@ -171,6 +181,7 @@ Partial Class FormListeFiche
         Me.BackgroundImage = Global.ProjetProcessusUnifier.My.Resources.Resources.degrade_gris_bleu_1095_593
         Me.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
         Me.ClientSize = New System.Drawing.Size(984, 498)
+        Me.Controls.Add(Me.Label3)
         Me.Controls.Add(Me.Label2)
         Me.Controls.Add(Me.btnVoirFiche)
         Me.Controls.Add(Me.pbxAtt)
@@ -200,4 +211,5 @@ Partial Class FormListeFiche
     Friend WithEvents VILLE As DataGridViewTextBoxColumn
     Friend WithEvents REPONSE4 As DataGridViewTextBoxColumn
     Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
 End Class
